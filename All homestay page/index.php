2d69fe858681e349772cedd26156f5a9c413a2b2
moen951 <ahmadamirul2@gmail.com>
2018-02-26 07:56:31 +0800

    add simple signup page

--- All homestay page/index.php
+++ All homestay page/index.php
@@ -55,25 +55,33 @@ window.onclick = function(event) {
 
 <div id="id02" class="modal">
   <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
-  <form class="modal-content animate" action="/action_page.php">
+  <form class="modal-content animate" action="signup.inc.php" method="GET">
     <div class="container">
       <h1>Sign Up</h1>
       <p>Please fill in this form to create an account.</p>
       <hr>
-      <label for="email"><b>Email</b></label>
-      <input type="text" placeholder="Enter Email" name="email" required>
+
+      <label for="userIC"><b>Identity Card Number(NRIC)</b></label>
+      <input type="text" placeholder="Enter Identity Card Number(NRIC)" name="userIC" required>
+
+      <label for="username"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="username" required>
 
       <label for="psw"><b>Password</b></label>
       <input type="password" placeholder="Enter Password" name="psw" required>
 
-      <label for="psw-repeat"><b>Repeat Password</b></label>
-      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-      
-      <label>
-        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-      </label>
+      <label for="psw2"><b>Repeat Password</b></label>
+      <input type="password" placeholder="Repeat Password" name="psw2" required>
 
+      
+      <label for="email"><b>Email</b></label>
+      <input type="text" placeholder="Enter Email" name="email" required>
+      
+      
+      <!--
       <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+      -->
 
       <div class="clearfix">
         <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
---
+++ All homestay page/signup.inc.php
@@ -0,0 +1,33 @@
+<?php
+//session_start();  
+include 'dbconnection.php';
+$userIC = $_GET['userIC'];
+$username = $_GET['username'];
+$email = $_GET['email'];
+$psw = $_GET['psw'];
+$psw2 =$_GET['psw2'];
+
+
+if ($psw==$psw2) 
+{
+	$sql ="INSERT INTO users (userIC, username, pword, email,role) 
+	VALUES ('$userIC', '$username', '$psw', '$email', '2')";
+	$result = $conn->query($sql);
+
+	header("refresh:0 url=index.php");
+						echo "<script>
+								
+   								 alert('Signup Successfully, Please Login Again');
+									
+									</script>";
+					}
+
+else
+{
+echo "<script>
+								
+   	alert('Password Not Match');
+									
+	</script>";
+
+}
