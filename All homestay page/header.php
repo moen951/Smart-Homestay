c3218d7ba26e7a8527b08fcbdaa67c52f69c116d
moen951 <ahmadamirul2@gmail.com>
2018-03-03 01:08:36 +0800

    made booking inside calendar

--- All homestay page/header.php
+++ All homestay page/header.php
@@ -66,21 +66,26 @@ $(document).ready(function()
 
         googleCalendarApiKey: 'AIzaSyAR9u_JmIPARI03Ove5Rk6ssvLmJYi6ODI',
 
-         events:'en.malaysia#holiday@group.v.calendar.google.com',
+
+
+         events2:'en.malaysia#holiday@group.v.calendar.google.com',
         header: {
-        left: "prev,next today",
-        center: "title",
+        right: "prev,next today",
+       
        
         
         },
-      defaultDate: "2018-02-10",
+      
       weekNumbers: true,
       navLinks: true, 
       editable: false,
       weekmode:'liquid',
       eventLimit: true,
+       events: 'load.php',
+    selectable:true,
+    selectHelper:true,
 
-      eventClick: function(event) {
+     eventClick: function(event) {
         // opens events in a popup window
         window.open(event.url, 'gcalevent', 'width=700,height=600');
         return false;
@@ -88,13 +93,89 @@ $(document).ready(function()
 
       loading: function(bool) {
         $('#loading').toggle(bool);
+      },
+
+    select: function(start, end, allDay)
+    {
+     var title = prompt("Enter Event Title");
+     if(title)
+     {
+      var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
+      var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
+      $.ajax({
+       url:"insert.php",
+       type:"POST",
+       data:{title:title, start:start, end:end},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Added Successfully");
+       }
+      })
+     }
+    },
+    editable:true,
+    eventResize:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function(){
+       
+       calendar.fullCalendar('refetchEvents');
+       alert('Event Update');
       }
+     })
+    },
 
+    eventDrop:function(event)
+    {
+     var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
+     var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
+     var title = event.title;
+     var id = event.id;
+     $.ajax({
+      url:"update.php",
+      type:"POST",
+      data:{title:title, start:start, end:end, id:id},
+      success:function()
+      { 
+       calendar.fullCalendar('refetchEvents');
+       alert("Event Updated");
+      }
+     });
+    },
 
+    eventClick:function(event)
+    {
+     if(confirm("Are you sure you want to remove it?"))
+     {
+      var id = event.id;
+      $.ajax({
+       url:"delete.php",
+       type:"POST",
+       data:{id:id},
+       success:function()
+       {
+        calendar.fullCalendar('refetchEvents');
+        alert("Event Removed");
+       }
+      })
+     }
+    },
+
+   });
+
+     $('#calendar').fullCalendar('addEventSource', 'en.malaysia#holiday@group.v.calendar.google.com');
 
     });
 
-});
+
 </script>
 
 <style>
