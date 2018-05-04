56619257ded3e39fcf92f8a11fd6dd8f0d745591
moen951 <ahmadamirul2@gmail.com>
2018-05-04 20:47:22 +0800

    solve the bootstrap

--- All homestay page/footer.php
+++ All homestay page/footer.php
@@ -73,30 +73,6 @@
 
 
 
-<script src="assets/jquery.js"></script>
-
-<!-- wow script -->
-<script src="assets/wow/wow.min.js"></script>
-
-<!-- uniform -->
-<script src="assets/uniform/js/jquery.uniform.js"></script>
-
-
-<!-- boostrap -->
-<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
-
-<!-- jquery mobile -->
-<script src="assets/mobile/touchSwipe.min.js"></script>
-
-<!-- jquery mobile -->
-<script src="assets/respond/respond.js"></script>
-
-<!-- gallery -->
-<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
-
-
-<!-- custom script -->
-<script src="assets/script.js"></script>
 
 
 
--- All homestay page/header.php
+++ All homestay page/header.php
@@ -10,6 +10,31 @@ if(!isset($_SESSION))
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <script src="../assets/jquery.js"></script>
+
+<!-- wow script -->
+<script src="../assets/wow/wow.min.js"></script>
+
+<!-- uniform -->
+<script src="../assets/uniform/js/jquery.uniform.js"></script>
+
+
+<!-- boostrap -->
+<script src="../assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="../assets/mobile/touchSwipe.min.js"></script>
+
+<!-- jquery mobile -->
+<script src="../assets/respond/respond.js"></script>
+
+<!-- gallery -->
+<script src="../assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+
+<!-- custom script -->
+<script src="../assets/script.js"></script>
+
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
